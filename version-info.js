5080b1d2002683a1d1b96785aa5b6aaa6921d49d
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-07-05 17:44:32 -0600

    feat: consultation types

--- version-info.js
+++ version-info.js
@@ -1,10 +1,10 @@
 // Auto-generated version information
-// Generated at: 2025-07-05T23:03:47.627Z
+// Generated at: 2025-07-05T23:33:26.424Z
 export const VERSION_INFO = {
   "version": "0.1.0",
-  "gitHash": "3f2b743",
+  "gitHash": "1c36677",
   "gitBranch": "dev",
-  "gitDate": "2025-07-05 16:59:14 -0600",
-  "buildDate": "2025-07-05T23:03:47.627Z",
-  "buildTimestamp": 1751756627627
+  "gitDate": "2025-07-05 17:05:10 -0600",
+  "buildDate": "2025-07-05T23:33:26.424Z",
+  "buildTimestamp": 1751758406429
 };
